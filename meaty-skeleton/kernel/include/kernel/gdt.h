7fc9d5cda71cc7bd9ce5cadec0579cf4de6a71c7
Jason Than <thanja@oregonstate.edu>
2026-05-03 15:45:08 -0700

    Added idt_entry set up

--- meaty-skeleton/kernel/include/kernel/gdt.h
+++ meaty-skeleton/kernel/include/kernel/gdt.h
@@ -12,18 +12,24 @@ typedef struct {
     uint8_t  access_rights;
     uint8_t  flags_limit_hi;
     uint8_t  base_xhi;
-} __attribute__((packed)) segment_descriptor_struct;
+} __attribute__((packed)) gdt_entry;
 
 typedef struct {
     uint16_t limit;
     uint32_t base;
-} __attribute__((packed)) gdt_ptr_struct;
+} __attribute__((packed)) gdt_descriptor;
+
+// typedef enum {
+//     // Accessed bit
+
+
+// } ACCESS_BYTES_AND_FLAGS;
 
 void init_gdt_segments();
-void create_segment_descriptor(segment_descriptor_struct* target, uint32_t base, uint32_t limit, uint8_t access_rights);
+void set_gdt_entry(gdt_entry* target, uint32_t base, uint32_t limit, uint8_t access_rights);
 
 // gdt.s
-extern void setGdt(gdt_ptr_struct*);
+extern void setGdt(gdt_descriptor*);
 extern void reloadSegments();
 
 #endif
